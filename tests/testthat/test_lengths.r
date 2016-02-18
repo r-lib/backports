44b02be721970b24c47fb15ebc52cd7622c6476e
Michel <michellang@gmail.com>
2016-02-18 16:42:25 +0100

    added strrep tests

--- tests/testthat/test_lengths.r
+++ tests/testthat/test_lengths.r
@@ -4,8 +4,8 @@ test_that("lengths()", {
   if (!exists("lengths", envir = baseenv()))
     skip("lengths() not found in base")
   f = get("lengths", envir = baseenv())
-
   expect_same = makeCompareFun(f, lengths)
+
   expect_same(1:3)
   expect_same(setNames(1:3, letters[1:3]))
   expect_same(setNames(1:3, letters[1:3]), use.names = FALSE)
