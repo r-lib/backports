910767ee189c63598b362c199e964d0d870927a6
Michel <michellang@gmail.com>
2016-02-18 16:33:19 +0100

    updated description

--- R/zzz.r
+++
@@ -1,5 +0,0 @@
-#' @description
-#' This package provides backports of functions which have been introduced in one of the base packages in R version 3.0.1 or later.
-#' These backports are conditionally exported in order to let R resolve the function name to either the implemented backport, or the respective base version, if available.
-#' This way, package developers can make use of the new, optimized functions without without worrying about the minimum R version required.
-"_PACKAGE"
