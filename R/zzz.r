e8cea45bac706649aae1aecfe36f80a7ef98296c
Michel <michellang@gmail.com>
2016-02-18 11:56:06 +0100

    cleanups

--- R/zzz.r
+++ R/zzz.r
@@ -1,4 +1,5 @@
-#' This package provides functions which have been introduced in one of the base packages in R version 3.0.1 or later.
-#' These backports are conditionally exported in order to let R resolve the function name to the implemented backport only if the respective base version is not available.
-#' As a package developer, you can just import \code{backports} to use all implemented backports without worrying about the required R version.
+#' @description
+#' This package provides backports of functions which have been introduced in one of the base packages in R version 3.0.1 or later.
+#' These backports are conditionally exported in order to let R resolve the function name to either the implemented backport, or the respective base version, if available.
+#' This way, package developers can make use of the new, optimized functions without without worrying about the minimum R version required.
 "_PACKAGE"
