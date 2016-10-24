b24ddad9896b31c608e66f1ab1823ea37cfbfc4b
Michel Lang <michellang@gmail.com>
2016-10-24 11:31:37 +0200

    fix test for windows

--- tests/testthat/test_file.info.R
+++ tests/testthat/test_file.info.R
@@ -4,7 +4,9 @@ test_that("file.info()", {
   x = tempdir()
   res1 = file.info(x, extra_cols = TRUE)
   res2 = file.info(x, extra_cols = FALSE)
-  expect_data_frame(res1, nrow = 1L, ncol = 10L)
+  expect_data_frame(res1, nrow = 1L, min.cols = 7L)
   expect_data_frame(res2, nrow = 1L, ncol = 6L)
+
+  expect_identical(res1, base::file.info(x))
   expect_identical(res1[, 1:6, drop = FALSE], res2)
 })
