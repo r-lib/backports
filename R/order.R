1a896c4c56edb6527db6b786bec468a6dc3408e3
Michel Lang <michellang@gmail.com>
2016-10-31 15:35:40 +0100

    backports for sort and order

---
+++ R/order.R
@@ -0,0 +1,18 @@
+#' @title Backport of \code{order} for R < 3.3.0
+#'
+#' @description
+#' Allows value \code{"radix"} for argument \code{method}.
+#' See the original description in \code{base::order}.
+#'
+#' @keywords internal
+#' @rawNamespace if (getRversion() < "3.3.0") export(order)
+#' @examples
+#' # get function from namespace instead of possibly getting
+#' # implementation shipped with recent R versions:
+#' bp_order = getFromNamespace("order", "backports")
+#'
+#' x = runif(10)
+#' data.frame(x = x, order = bp_order(x, method = "radix"))
+order = function (..., na.last = TRUE, decreasing = FALSE, method = c("shell", "radix")) {
+  base::order(..., na.last = na.last, decreasing = decreasing, method = "shell")
+}
---
+++ R/sort.R
@@ -0,0 +1,19 @@
+#' @title Backport of \code{sort} for R < 3.3.0
+#'
+#' @description
+#' Allows value \code{"radix"} for argument \code{method}.
+#' See the original description in \code{base::sort}.
+#'
+#' @keywords internal
+#' @rawNamespace if (getRversion() < "3.3.0") export(sort)
+#' @examples
+#' # get function from namespace instead of possibly getting
+#' # implementation shipped with recent R versions:
+#' bp_sort = getFromNamespace("sort", "backports")
+#'
+#' bp_sort(runif(10), method = "radix")
+sort = function (x, decreasing = FALSE, method = c("shell", "quick", "radix"), ...) {
+  if (identical(method, "radix"))
+    method = "shell"
+  base::sort(x, decreasing = FALSE, method = method, ...)
+}
