0ea4a0bf364e51aad463d1d44cb749c8e1a987e9
Michel Lang <michellang@gmail.com>
2017-05-19 14:08:21 +0200

    cleanup

--- R/import.R
+++ R/import.R
@@ -6,10 +6,10 @@
 #' See examples for a code snippet to add to your package to do this during load-time.
 #'
 #' @param pkgname [\code{character(1)}]\cr
-#'   Name of the package where the function should be assigned.
+#'   Name of the package where the backported function should be assigned.
 #' @param obj [\code{character}]\cr
 #'   Name of objects to assign, as character vector.
-#'   If \code{NULL}, all backports which are not provided by R are assigned.
+#'   If \code{NULL}, all backports which are not provided by R itself are assigned.
 #' @export
 #' @seealso \code{\link[base]{.onLoad}}
 #' @examples
@@ -23,7 +23,7 @@
 #' .onLoad <- function(libname, pkgname) {
 #'   backports::import(pkgname, "trimws")
 #' }
-#'}
+#' }
 import = function(pkgname, obj = NULL) {
   pkg = getNamespace(pkgname)
   backports = getNamespace("backports")
@@ -34,15 +34,15 @@ import = function(pkgname, obj = NULL) {
   }
 
   if (!is.null(obj)) {
-    bp.base = intersect(bp.base, obj)
-    bp.utils = intersect(bp.utils, obj)
+    BASE = intersect(BASE, obj)
+    UTILS = intersect(UTILS, obj)
   }
 
-  lapply(bp.base, assignIfNotExists, where = baseenv())
-  lapply(bp.utils, assignIfNotExists, where = getNamespace("utils"))
+  lapply(BASE, assignIfNotExists, where = baseenv())
+  lapply(UTILS, assignIfNotExists, where = getNamespace("utils"))
   invisible(TRUE)
 }
 
-bp.base = c("anyNA", "dir.exists", "endsWith", "file.info", "file.mode", "file.mtime", "file.size", "get0", "lengths", "startsWith", "strrep", "trimws")
-
-bp.utils = "hasName"
+# constants used in import()
+BASE = c("anyNA", "dir.exists", "endsWith", "file.info", "file.mode", "file.mtime", "file.size", "get0", "lengths", "startsWith", "strrep", "trimws")
+UTILS = "hasName"
