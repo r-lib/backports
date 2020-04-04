0e17a2f9634be6a0e2f02b2875c49637f3a1a3b1
Michel Lang <michellang@gmail.com>
2020-04-04 15:35:44 +0200

    fix dQuote test

--- tests/test_dQuote.R
+++ tests/test_dQuote.R
@@ -4,13 +4,17 @@ f = get("dQuote", envir = baseenv())
 expect_same = makeCompareFun(f, backports:::dQuote)
 
 expect_same("foo")
-expect_same("foo", q = TRUE)
-expect_same("foo", q = FALSE)
+if (getRversion() >= "3.6.0") {
+  expect_same("foo", q = TRUE)
+  expect_same("foo", q = FALSE)
+}
 
 
 f = get("sQuote", envir = baseenv())
 expect_same = makeCompareFun(f, backports:::sQuote)
 
 expect_same("foo")
-expect_same("foo", q = TRUE)
-expect_same("foo", q = FALSE)
+if (getRversion() > "3.6.0") {
+  expect_same("foo", q = TRUE)
+  expect_same("foo", q = FALSE)
+}
