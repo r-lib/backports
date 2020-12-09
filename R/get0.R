2d4c2edd1d67d96fefb705167b56c10a8bdb5fa3
Michel Lang <michellang@gmail.com>
2020-12-09 12:36:12 +0100

    Fix get0 for r-devel (#54)

--- R/get0.R
+++ R/get0.R
@@ -17,7 +17,14 @@
 #' foo = 12
 #' bp_get0("foo")
 get0 = function(x, envir = pos.to.env(-1L), mode = "any", inherits = TRUE, ifnotfound = NULL) {
-  if (!is.character(x) || length(x) == 0L)
+  if (!is.character(x) || length(x) == 0L) {
     stop("Invalid first argument")
+  }
+  if (length(x) > 1L && getRversion() >= "4.1.0") {
+    # this check was introduced in R-4.1.0
+    # We can remove the version check as soon as all CRAN packages have been updated,
+    # i.e. as soon as 4.1.0 is released
+    stop("first argument has length > 1")
+  }
   mget(x[1L], envir = envir, mode = mode, inherits = inherits, ifnotfound = list(ifnotfound))[[1L]]
 }
