99116192d7c9e31222a693a23438d594ede9c6fc
Michel Lang <michellang@gmail.com>
2021-10-25 11:17:19 +0200

    fix test for r devel

--- tests/test_list2DF.R
+++ tests/test_list2DF.R
@@ -4,7 +4,7 @@ if (exists("list2DF", envir = baseenv())) {
   f = get("list2DF", envir = baseenv())
   expect_same = makeCompareFun(f, backports:::list2DF)
 
-  x <- list(character(), "A", c("B", "C"))
+  x <- list(c("A", "B"), c("B", "C"))
   n <- lengths(x)
   expect_same(list(x = x, n = n))
 
