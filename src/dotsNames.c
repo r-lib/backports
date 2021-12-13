6a0f68c5af5f3d3e996f51d1274dc8052fdcd3ea
Michel Lang <michellang@gmail.com>
2021-12-13 11:37:01 +0100

    ...names() patched

--- src/dotsNames.c
+++ src/dotsNames.c
@@ -6,18 +6,27 @@
 SEXP dotsNames(SEXP env) {
     SEXP vl = findVar(R_DotsSymbol, env);
     PROTECT(vl);
-    if (vl == R_UnboundValue) {
-        error("incorrect context: the current call has no '...' to look in");
-    }
-
-    SEXP out = PROTECT(allocVector(STRSXP, length_DOTS(vl)));
-    for(int i = 0; i < LENGTH(out); i++) {
-        SEXP tag = TAG(vl);
-        SET_STRING_ELT(out, i, tag == R_NilValue ? NA_STRING : PRINTNAME(tag));
+    if (vl == R_UnboundValue)
+    error("incorrect context: the current call has no '...' to look in");
+    // else
+    SEXP out;
+    int n = length_DOTS(vl);
+    Rboolean named = FALSE;
+    for(int i = 0; i < n; i++) {
+        if(TAG(vl) != R_NilValue) {
+            if(!named) { named = TRUE;
+                PROTECT(out = allocVector(STRSXP, n)); // and is filled with "" already
+            }
+            SET_STRING_ELT(out, i, PRINTNAME(TAG(vl)));
+        }
         vl = CDR(vl);
     }
-
-    UNPROTECT(2); /* ans, vl */
+    if(named) {
+        UNPROTECT(1);
+    } else {
+        out = R_NilValue;
+    }
+    UNPROTECT(1);
     return out;
 }
 
