4ed1a17b40a4e9a60bdb75deaca6c718403fdd37
Michel Lang <michellang@gmail.com>
2026-04-02 10:39:19 +0200

    start fixing c api calls

--- src/dotsLength.c
+++ src/dotsLength.c
@@ -1,9 +1,18 @@
 #include <R.h>
 #include <Rinternals.h>
+#include <Rversion.h>
 
 SEXP dotsLength(SEXP env_) {
-    SEXP ddd = findVar(R_DotsSymbol, env_);
-    if (ddd == R_UnboundValue)
-        error("incorrect context: the current call has no '...' to look in");
+    #if defined(R_VERSION) && R_VERSION >= R_Version(4, 6, 0)
+        if (!R_DotsExist(env_)) {
+            error("incorrect context: the current call has no '...' to look in");
+        }
+        return R_DotsLength(env_);
+    #else
+        SEXP ddd = findVar(R_DotsSymbol, env_);
+        if (ddd == R_UnboundValue)
+            error("incorrect context: the current call has no '...' to look in");
+    #endif
+
     return ScalarInteger(TYPEOF(ddd) == DOTSXP ? length(ddd) : 0);
 }
